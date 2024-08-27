b8ee0da3d886f171018f3f05a23bf36946ba2abc
lucasCarneiros <lucas.carneirocontact@gmail.com>
2024-08-27 15:20:06 -0300

    add

--- src/main/java/br/com/projeto/api/modelo/Pessoa.java
+++ src/main/java/br/com/projeto/api/modelo/Pessoa.java
@@ -1,7 +1,20 @@
 package br.com.projeto.api.modelo;
 
-public class Pessoa {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity //Espifica a criacao da tabela do BD, nesse caso a tabela sera pessoa
+
+@Table(name = "pessoas") //Usamos essa anotacao apenas quando queremos que o nome da tabela seja diferente do nome da classe
+
+public class Pessoa {
+    
+    @Id //Cria a chave primaria nesse caso sera o atributo abaixo codigo
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Vai gerar valores de maneira crescente para a chave primaria iniciando no valor 1
+    private int codigo; //Responsavel pela chave primaria
     private String nome;
     private int idade;
     
@@ -16,6 +29,12 @@ public class Pessoa {
     }
     public void setIdade(int idade) {
         this.idade = idade;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
     }
     
 }
